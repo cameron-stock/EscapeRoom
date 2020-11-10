c53f77f9690dcc5fee81b8325bf19607959880d6
Cameron-Stock <clstock33@gmail.com>
2020-11-09 20:49:52 -0600

    Added new colors, verbiage, and fixed scorechart

--- EscapeRoom/Program.cs
+++ EscapeRoom/Program.cs
@@ -8,6 +8,8 @@ namespace EscapeRoom
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
 
+            bool playAgain = true;
+
             Console.WriteLine("Hello, and welcome to the ESCAPE ROOM! **SPookY HallowEEN EdiTIOn**");
            
             Console.WriteLine("First, you will be asked a few questions...");
@@ -21,30 +23,51 @@ namespace EscapeRoom
             Console.WriteLine($"{color} is an awesome color! What is your favorite halloween candy?");
             var candy = Console.ReadLine();
 
-            Console.WriteLine($"{userName}, that sounds gross! What is your favorite costume?");
+            Console.WriteLine($"Ewww {userName}, that sounds gross! What is your favorite halloween costume?");
             var toy = Console.ReadLine();
 
             Console.WriteLine($"Thanks, {userName}! Look below, The game is about to begin!");
+
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine("------------------------------------------");
 
-            // Console.WriteLine($"Name: {userName}");
-            // Console.WriteLine($"Favorite toy: {toy}");
-            // Console.WriteLine($"Favorite candy: {candy}");
-            // Console.WriteLine($"Favorite color: {color}");
+            Console.BackgroundColor = ConsoleColor.Gray;
+
+            Console.ForegroundColor = ConsoleColor.Black;
 
             Console.WriteLine($"{userName}, you just entered the ESCAPE ROOM... Are you nervous?");
             Console.ReadLine();
-            Console.WriteLine($"I bet so, all you have with you is your toy {toy}, and a handful of {candy}s. Are you ready??");
-            Console.ReadLine();
+            Console.WriteLine($"I bet so, you are dressed up as {toy}, with a handful of {candy}. Are you ready?? (Y/N) ");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+
+            if ( input == "Y")
+            {
+                playAgain = true;
+                Console.Clear();
+            }
+            else if (input == "N")
+            {
+                playAgain = false;
+                Console.WriteLine($"Well finish eating your {candy}, and lets play!!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry!!!");
+            }
+
+            Console.WriteLine($"Okay {userName}, Here is how it works..");
+
             Console.WriteLine("You have to choose between escaping through the DOOR, VENT or the WINDOW. The only problem is, you do not know what's on the other side!!  ");
-            Console.WriteLine("The Escape room will try to guess which way you are trying to escape!! Don't let it catch you!");
+            Console.WriteLine($"The Escape room will try to guess which way you are trying to escape!! Don't let it catch you {userName}!");
+            Console.WriteLine("Type either Door, Vent, or Window to decide where you will try to escape!");
 
 
             string inputUser, inputPc;
             int randomInt;
 
 
-            bool playAgain = true;
+          
 
             while (playAgain)
             {
@@ -53,6 +76,7 @@ namespace EscapeRoom
                 int pcScore = 0;
 
                 // Scoring system is based on first "player" to reach 3 Points
+                // I can change how many chances the user has to try to escape
 
                 while (userScore < 3 && pcScore < 3)
                 {
@@ -75,6 +99,7 @@ namespace EscapeRoom
                             if (inputUser == "DOOR")
                             {
                                 Console.WriteLine("The Escape Room caught you!\n\n");
+                                pcScore++;
                             }
                             else if (inputUser == "VENT")
                             {
@@ -83,7 +108,7 @@ namespace EscapeRoom
                             }
                             else if (inputUser == "WINDOW")
                             {
-                                Console.WriteLine("The Window is locked! The Escape room caught you!\n\n");
+                                Console.WriteLine("But the Window is locked! The Escape room caught you!\n\n");
                                 pcScore++;
                             }
                             break;
@@ -94,11 +119,12 @@ namespace EscapeRoom
                             if (inputUser == "DOOR")
                             {
                                 Console.WriteLine("Nice, You successfully snuck out the door!\n\n");
-                                pcScore++;
+                                userScore++;
                             }
                             else if (inputUser == "VENT")
                             {
                                 Console.WriteLine("BUSTED!!! The Escape Room caught you!\n\n");
+                                pcScore++;
                             }
                             else if (inputUser == "WINDOW")
                             {
@@ -118,11 +144,12 @@ namespace EscapeRoom
                             else if (inputUser == "VENT")
                             {
                                 Console.WriteLine("GOOD THINKING! You snuck out of the vent!\n\n");
-                                pcScore++;
+                                userScore++;
                             }
                             else if (inputUser == "WINDOW")
                             {
                                 Console.WriteLine("BUSTED!! The Escape Room caught you trying to slip out the Window!!!\n\n");
+                                pcScore++;
                             }
                             break;
                         default:
@@ -130,7 +157,7 @@ namespace EscapeRoom
                             break;
 
                     }
-                    Console.WriteLine("\n\nScores:\tPlayer:\t{0}\tComputer:\t{1}\t", userScore, pcScore);
+                    Console.WriteLine("\n\nScores:\tPlayer:\t{0}\tEscape Room:\t{1}\t", userScore, pcScore);
                 }
 
                 if (userScore == 3)
